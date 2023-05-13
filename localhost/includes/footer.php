b9b603c7ccfdfb8ae5c1500cea0934dd9071f982
helliong <90344433+helliong@users.noreply.github.com>
2023-05-13 19:34:46 +0600

    Delete localhost directory

--- localhost/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <hr>
-    <p>&copy;Тренировка, 2023</p>
-</footer>
-
-
-</body>
-</html>
--- localhost/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'includes/core.php';
-unset($_SESSION['auth_user_id']);
-redirect('index.php');
